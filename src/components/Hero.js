a8eea62a7bc3dc3b7a1554edd599f6cc0f408f64
Mheyrie <omota4fidelity@gmail.com>
2024-05-07 23:51:26 +0100

    Hero section commence

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,7 @@ function Hero() {
   return (
     <section>
       <h1>Borrow/Lend</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi eaque deserunt sapiente voluptatibus laboriosam nulla repellendus molestias repudiandae exercitationem aut similique alias minus vero, neque eligendi nam non? Architecto, magnam esse. Pariatur, qui. Velit asperiores quas, quod sequi earum eos eum eveniet nisi eligendi hic? Maiores harum blanditiis dolore illo.</p>
     </section>
   )
 }
