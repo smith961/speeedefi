888c355143405fd8c0a9c846ab33000b176c5b45
smith <smithbabawande@gmail.com>
2024-05-08 13:48:00 +0100

    Started working on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,24 @@
 
 import React from 'react'
+import styled from 'styled-components'
 
 function Footer() {
   return (
-    <div>Footer</div>
+   <Wrapper>
+    <div>FOOTER</div>
+
+   </Wrapper>
   )
 }
+
+const Wrapper = styled.header`
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ margin: 0;
+ background: #0C2C59;
+ width: auto;
+ color: whitesmoke;
+ `
 
 export default Footer
