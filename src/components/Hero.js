e8a00f05bae604a5e341ac9e90089bbfdd3821b4
smith <smithbabawande@gmail.com>
2024-05-09 17:59:12 +0100

    working on footer

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import SupplyLend from '../pages/SupplyLend'
+import styled from 'styled-components'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    
+    <HeroDiv>
+ <SupplyLend />
+    </HeroDiv>
+   
+    
+    
+  
   )
 }
 
+
 export default Hero
+
+const HeroDiv = styled.div`
+height: 50vh;
+width: 100%;
+background-color: red;
+`
--- src/pages/SupplyLend.js
+++ src/pages/SupplyLend.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const SupplyLend = () => {
   return (
-    <div>SupplyLend</div>
+    <Wrapper>
+      SUPPLYLEND
+      </Wrapper>
   )
 }
 
 export default SupplyLend
+
+const Wrapper = styled.header`
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ margin: 0;
+ background: #0C2C59;
+ width: auto;
+ color: whitesmoke;
+ `
